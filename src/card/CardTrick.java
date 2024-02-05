c8b84de2124dc364e07350577a47d3c7e3c29310
Rajmohinder kaur <Rajmohinder kaur@DESKTOP-R5PRAV4>
2024-02-04 21:29:08 -0500

    cards

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -17,18 +19,46 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(random.nextInt(13) + 1);  // Random value between 1 and 13
+
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[random.nextInt(4)]);  // Random suit index between 0 and 3
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.print("Pick a card value (1-13): ");
+        int userValue = scanner.nextInt();
+        System.out.print("Pick a card suit (hearts, diamonds, clubs, spades): ");
+        String userSuit = scanner.next().toLowerCase();
+
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(userSuit);
+
+
+
         // and search magicHand here
-        //Then report the result here
+        boolean found = false;
+        for (Card card : magicHand) {
+            if (card.equals(userCard)) {
+                found = true;
+                break;
+            }
+        }
+            //Then report the result here
+         if (found) {
+            System.out.println("Yeah! Your card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
         // add one luckcard hard code 2,clubs
+        }
+
     }
-    
 }
